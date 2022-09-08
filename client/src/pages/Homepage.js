770a93b3f4c2a03d77f1c45981340a1ca8a5af3a
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-09-08 12:43:30 +0530

    fixed react bug of router

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <>
+    <h1>Hello Welcome to my Blog</h1>
+    <p>Set in Kyoto 2027, the Japanese government has made plans to collect and preserve the city's natural architecture and culture through drones in real time, storing all its data in an infinite-capacity quantum computer known as Alltale. Naomi Katagaki is an indecisive high school student living in Kyoto who harbors a love for reading. One day after school, a mysterious yatagarasu steals his library book and in an attempt to get it back, he meets a strange man that appears out of nowhere. This man, whom only Naomi can see, is revealed to be himself from 10 years later, now grown up and an adult. The adult Naomi explains </p>
+    <p>that he has accessed Alltale from the real world outside in order to change the recorded past and save Naomi's to-be girlfriend, his classmate Ruri Ichigyō, after she was unfortunately struck by lightning at a fireworks festival and rendered comatose. The teenag</p>
+    </>
+  )
+}
+
